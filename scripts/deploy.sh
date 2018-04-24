0ef18e151782f30faa3985a8c0d5a6d3d05cefa2
Jake Bolam <jake.bolam@gmail.com>
2018-04-24 08:41:32 -0400

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,12 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/shell-helpers.sh"
 
-version=$( git describe | cut -d '-' -f 1 | cut -d 'v' -f 2 )
-version_length=${#version}
+version=$(git describe)
+version=$( echo $version | cut -d 'v' -f 2 )
 
 # 0.0.35 6 char max
-if [ $veversion_lengthrsion -gt 6 ]; then
+version_length=${#version}
+if [ $version_length -gt 6 -o $version_length -lt 5 ]; then
     echo "${version} does not appear to be a release tag"
     echo "Not deploying."
     exit 0
@@ -21,7 +22,7 @@ echo '//registry.npmjs.org/:_authToken=${NPM_PUBLISH_TOKEN}' > .npmrc
 PACKAGE_VERSION_JSON=package.json
 PACKAGE_VERSION_NPM=@bundlesize/bundlesize
 
-jq ".version=\"$VERSION_TO_PUBLISH\"" $PACKAGE_VERSION_JSON > $PACKAGE_VERSION_JSON.tmp
+jq ".version=\"$version\"" $PACKAGE_VERSION_JSON > $PACKAGE_VERSION_JSON.tmp
 mv $PACKAGE_VERSION_JSON.tmp $PACKAGE_VERSION_JSON
 
 cat $PACKAGE_VERSION_JSON
