be38bb55c3a1a314645c1a3ac7223c54af1767f9
Jake Bolam <jake.bolam@gmail.com>
2018-04-24 08:31:59 -0400

    deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,19 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/shell-helpers.sh"
 
+version=$( git describe | cut -d '-' -f 1 | cut -d 'v' -f 2 )
+version_length=${#version}
+
+# 0.0.35 6 char max
+if [ $veversion_lengthrsion -gt 6 ]; then
+    echo "${version} does not appear to be a release tag"
+    echo "Not deploying."
+    exit 0
+fi
+
+echo "${version} appears to be a release tag"
+echo "Deploying...."
+
 echo '//registry.npmjs.org/:_authToken=${NPM_PUBLISH_TOKEN}' > .npmrc
 
 PACKAGE_VERSION_JSON=package.json
@@ -14,3 +27,5 @@ mv $PACKAGE_VERSION_JSON.tmp $PACKAGE_VERSION_JSON
 cat $PACKAGE_VERSION_JSON
 
 #run "npm publish --access public"
+
+echo "Deployed!"
