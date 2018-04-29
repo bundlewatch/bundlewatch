8bfb5a8e1fda6e0223c15778b1ffe3c7bec75c90
Jake Bolam <jake.bolam@gmail.com>
2018-04-28 21:32:53 -0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/shell-helpers.sh"
 
 version=$(git describe --tags)
-version=0.1.1
+version=0.1.2
 
 echo "${version} appears to be a release tag"
 echo "Deploying...."
