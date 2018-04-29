49b3691c15adde0696e6e25c7bafdaa164f3b167
Jake Bolam <jake.bolam@gmail.com>
2018-04-28 21:08:54 -0400

    all

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,12 +7,7 @@ version=$(git describe --tags)
 version=$( echo $version | cut -d 'v' -f 2 )
 
 # 0.0.35 6 char max
-version_length=${#version}
-if [ $version_length -gt 6 -o $version_length -lt 5 ]; then
-    echo "${version} does not appear to be a release tag"
-    echo "Not deploying."
-    exit 0
-fi
+version=0.1.1
 
 echo "${version} appears to be a release tag"
 echo "Deploying...."
@@ -25,8 +20,8 @@ PACKAGE_VERSION_NPM=@bundlesize/bundlesize
 jq ".version=\"$version\"" $PACKAGE_VERSION_JSON > $PACKAGE_VERSION_JSON.tmp
 mv $PACKAGE_VERSION_JSON.tmp $PACKAGE_VERSION_JSON
 
-cat $PACKAGE_VERSION_JSON
+#cat $PACKAGE_VERSION_JSON
 
-#run "npm publish --access public"
+run "npm publish --access public"
 
 echo "Deployed!"
