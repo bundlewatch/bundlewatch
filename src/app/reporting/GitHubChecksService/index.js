90b716f4db0b58a5034921ad3f2a0611083cc1ff
Jake Bolam <jake.bolam@gmail.com>
2018-05-12 12:24:45 -0400

    wip

---
+++ src/app/reporting/GitHubChecksService/index.js
@@ -0,0 +1,89 @@
+import axios from 'axios'
+
+import logger from '../../../logger'
+
+class GitHubChecksService {
+    constructor({ repoOwner, repoName, commitSha, githubAccessToken }) {
+        this.repoOwner = repoOwner
+        this.repoName = repoName
+        this.commitSha = commitSha
+        this.githubAccessToken = githubAccessToken
+        this.contexts = new Set()
+    }
+
+    get repo() {
+        return `${this.repoOwner}/${this.repoName}`
+    }
+
+    get enabled() {
+        if (
+            this.githubAccessToken &&
+            this.repoOwner &&
+            this.repoName &&
+            this.commitSha
+        ) {
+            return true
+        }
+
+        return false
+    }
+
+    update(message, url, status) {
+        if (!this.enabled) {
+            return Promise.resolve({})
+        }
+
+        const context = 'checks'
+
+        if (!this.contexts.has(context) && this.contexts.size >= 5) {
+            logger.warn(
+                `Max reported statuses reached, github status will not be reported`,
+            )
+            return Promise.resolve()
+        }
+        this.contexts.add(context)
+
+        return axios({
+            method: 'POST',
+            url: `https://api.github.com/repos/${this.repo}/statuses/${
+                this.commitSha
+            }`,
+            responseType: 'json',
+            data: {
+                state: status,
+                target_url: url,
+                description: message,
+                context,
+            },
+            timeout: 5000,
+            headers: {
+                Authorization: `token ${this.githubAccessToken}`,
+            },
+        }).catch(error => {
+            if (error.response) {
+                logger.error(
+                    `GitHubService HTTP_${error.response.status} :: ${
+                        error.response.data ? error.response.data.message : ''
+                    }`,
+                )
+                return
+            }
+            throw error
+        })
+    }
+
+    start({ message }) {
+        return this.update(message, undefined, 'pending')
+    }
+    pass({ message, url }) {
+        return this.update(message, url, 'success')
+    }
+    fail({ message, url, filePath }) {
+        return this.update(message, url, 'failure', filePath)
+    }
+    error({ message }) {
+        return this.update(message, undefined, 'error')
+    }
+}
+
+export default GitHubChecksService
