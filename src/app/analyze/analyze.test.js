747bbc8045a965c15b10859e2bc8eebabe6d941a
Francois Campbell <campbell.francois@gmail.com>
2018-05-26 11:05:22 -0400

    move mock data to separate file

--- src/app/analyze/analyze.test.js
+++ src/app/analyze/analyze.test.js
@@ -1,39 +1,5 @@
 import { getOverallDifference } from '.'
-
-const mockFileResults = {
-    oneFileLarger: [
-        {
-            size: 125,
-            baseBranchSize: 100,
-        },
-    ],
-    oneFileSmaller: [
-        {
-            size: 200,
-            baseBranchSize: 50,
-        },
-    ],
-    oneFileLargerOneFileSmallerOverallSmaller: [
-        {
-            size: 125,
-            baseBranchSize: 100,
-        },
-        {
-            size: 650,
-            baseBranchSize: 700,
-        },
-    ],
-    oneFileLargerOneFileSmallerOverallLarger: [
-        {
-            size: 225,
-            baseBranchSize: 100,
-        },
-        {
-            size: 650,
-            baseBranchSize: 700,
-        },
-    ],
-}
+import { mockFileResults } from './analyze.test.mockdata'
 
 describe('getOverallDifference', () => {
     it('Results match snapshot for the same input', () => {
