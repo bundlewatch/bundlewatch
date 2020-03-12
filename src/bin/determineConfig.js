ad1fee7f7fd79efe99f01700ceba262e96e0ddb5
Emmanuel Ogbizi <emmanuel.ogbizi-ugbe@tophatmonocle.com>
2020-03-12 07:11:42 -0400

    chore: address breaking change in v7

--- src/bin/determineConfig.js
+++ src/bin/determineConfig.js
@@ -52,7 +52,7 @@ const getConfigFileContents = configFilePath => {
 }
 
 const determineConfig = cliOptions => {
-    const pkgJson = (readPkgUp.sync() || {}).package
+    const pkgJson = (readPkgUp.sync() || {}).packageJson
     let pkgJsonbundlewatch = pkgJson.bundlewatch
 
     if (cliOptions.args && cliOptions.args.length > 0) {
